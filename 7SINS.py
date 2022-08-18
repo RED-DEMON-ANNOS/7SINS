63cbde77f7ab930f953f237d4aae1cd76dbd2353
Red Demon <105274882+RED-DEMON-ANNOS@users.noreply.github.com>
2022-08-18 17:02:14 +0500

    Update 7SINS.py

--- 7SINS.py
+++ 7SINS.py
@@ -1,2 +1,9 @@
-from zsins import menu
-menu()
+import os,platform
+os.system('git pull')
+ 
+bsn=platform.architecture()[0]
+if bsn=="32bit":
+    __import__("ss").menu()
+elif bsn=="64bit":
+    __import__("zsins").menu()
+ 
